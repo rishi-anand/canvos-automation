f51914834db2a69681690883a7085bc2d82bda18
Rishi <rishi.anand0@gmail.com>
2023-10-17 19:15:55 +0530

    temporary commit

--- .github/actions/canvos/canvos-build.sh
+++ .github/actions/canvos/canvos-build.sh
@@ -13,17 +13,17 @@ fi
 
 IFS='+' read -ra OS_PARTS <<< "$os_distribution"
 echo "CUSTOM_TAG=$custom_image_tag" >> .arg
-echo "IMAGE_REGISTRY=$IMAGE_REGISTRY" >> .arg
-echo "IMAGE_REPO=$IMAGE_REPO" >> .arg
+echo "IMAGE_REGISTRY=$image_registry" >> .arg
+echo "IMAGE_REPO=$image_repo" >> .arg
 echo "OS_DISTRIBUTION=${OS_PARTS[0]}" >> .arg
 echo "OS_VERSION=${OS_PARTS[1]}" >> .arg
 echo "K8S_DISTRIBUTION=$k8s_distribution" >> .arg
-echo "ISO_NAME=$ISO_NAME" >> .arg
-echo "ARCH=$ARCH" >> .arg
+echo "ISO_NAME=$iso_name" >> .arg
+echo "ARCH=$arch" >> .arg
 
-if [ -n "$BASE_IMAGE" ]; then
-  echo "The base image variable is not empty: $BASE_IMAGE"
-  echo "BASE_IMAGE=$BASE_IMAGE" >> .arg
+if [ -n "$base_image" ]; then
+  echo "The base image variable is not empty: $base_image"
+  echo "BASE_IMAGE=$base_image" >> .arg
 else
   echo "The base image var is empty"
 fi
