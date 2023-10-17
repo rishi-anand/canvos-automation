7774fbb4aa35e3a97fae2e6746e1c5ab2b59eb52
Rishi <rishi.anand0@gmail.com>
2023-10-17 20:12:12 +0530

    temporary commit

--- .github/actions/canvos/canvos-build.sh
+++ .github/actions/canvos/canvos-build.sh
@@ -3,30 +3,55 @@
 set -e
 set -x
 
-git clone https://github.com/spectrocloud/CanvOS.git
-cd CanvOS
-ls
-if [ -n "$canvos_tag" ]; then
-  echo "The environment variable is not empty: $canvos_tag"
-  git checkout $canvos_tag
-fi
-
 IFS='+' read -ra OS_PARTS <<< "$os_distribution"
-echo "CUSTOM_TAG=$custom_image_tag" >> .arg
-echo "IMAGE_REGISTRY=$image_registry" >> .arg
-echo "IMAGE_REPO=$image_repo" >> .arg
-echo "OS_DISTRIBUTION=${OS_PARTS[0]}" >> .arg
-echo "OS_VERSION=${OS_PARTS[1]}" >> .arg
-echo "K8S_DISTRIBUTION=$k8s_distribution" >> .arg
-echo "ISO_NAME=$iso_name" >> .arg
-echo "ARCH=$arch" >> .arg
 
-if [ -n "$base_image" ]; then
-  echo "The base image variable is not empty: $base_image"
-  echo "BASE_IMAGE=$base_image" >> .arg
-else
-  echo "The base image var is empty"
-fi
+function git_clone() {
+  git clone https://github.com/spectrocloud/CanvOS.git
+  cd CanvOS
+  ls
+  if [ -n "$canvos_tag" ]; then
+    echo "The environment variable is not empty: $canvos_tag"
+    git checkout $canvos_tag
+  fi
+}
 
-cat .arg
-./earthly.sh +build-all-images
+function create_arg_file() {
+  echo "CUSTOM_TAG=$custom_image_tag" >> .arg
+  echo "IMAGE_REGISTRY=$image_registry" >> .arg
+  echo "IMAGE_REPO=$image_repo" >> .arg
+  echo "OS_DISTRIBUTION=${OS_PARTS[0]}" >> .arg
+  echo "OS_VERSION=${OS_PARTS[1]}" >> .arg
+  echo "K8S_DISTRIBUTION=$k8s_distribution" >> .arg
+  echo "ISO_NAME=$iso_name" >> .arg
+  echo "ARCH=$arch" >> .arg
+
+  if [ -n "$base_image" ]; then
+    echo "The base image variable is not empty: $base_image"
+    echo "BASE_IMAGE=$base_image" >> .arg
+  else
+    echo "The base image var is empty"
+  fi
+
+  cat .arg
+}
+
+function login_gcr() {
+  echo $GCP_SPECTRO_DEV_PUBLIC_BASE64_ENCODED_JSON | base64 -d > /tmp/spectro-dev.json
+  docker login -u _json_key --password-stdin https://gcr.io < /tmp/spectro-dev.json
+}
+
+function build_artifacts() {
+  build_type
+  if [ "$build_type" = "ISO-Provider" ]; then
+    echo "Building ISO & Provider Images"
+    ./earthly.sh --push +build-all-images
+  elif [ "$build_type" = "Provider" ]; then
+    echo "Building only Provider Images"
+    ./earthly.sh --push +build-provider-images
+  fi
+}
+
+git_clone
+create_arg_file
+login_gcr
+build_artifacts
