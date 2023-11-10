58095970da3ad4446e6bced46c1c5f0dd68645b7
Rishi <rishi.anand0@gmail.com>
2023-11-10 21:05:45 +0530

    temporary commit

--- .github/actions/bdd-runner/run.sh
+++ .github/actions/bdd-runner/run.sh
@@ -7,17 +7,14 @@ WORKDIR=$(pwd)
 BDD_REPO=$WORKDIR/teams-edge-native
 echo "Github username is $github_user"
 
-#function git_clone_teams_edge_native() {
-#  git clone https://github.com/spectrocloud/teams-edge-native.git
-#  cd $BDD_REPO
-#}
-
-function info() {
+function git_clone_teams_edge_native() {
+  git clone https://$ACCESS_TOKEN@github.com/spectrocloud/teams-edge-native.git
+  cd $BDD_REPO
   pwd
   ls
 }
 
-info
+git_clone_teams_edge_native
 
 
 
